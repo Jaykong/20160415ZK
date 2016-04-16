f22337fbc6143c9647ccf88b6a17ebcada9e6ca0
Kyle Zhang <zhk108@icloud.com>
2016-04-16 13:18:46 +0800

    Exe2.2

---
+++ Exercise/Chap2/Exe2.2/Exe2.2/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Exe2.2
+//
+//  Created by Kyle on 16/4/16.
+//  Copyright © 2016年 Kyle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"In Objective-C,lowercase letters are significant.\n");
+        NSLog(@"Open and closed brances enclose program statements in a routine.\n");
+        NSLog(@"main is where program execution begins.\n");
+        NSLog(@"All pragram statemens must be terminated by a semicolon");
+    }
+    return 0;
+}
