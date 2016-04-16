756ca694a21d2d3d84ab7fc8253199fe3cfc8e3c
Kyle Zhang <zhk108@icloud.com>
2016-04-16 12:41:42 +0800

    add Exe2.1.3

---
+++ Exercise/Chap2/Exe2.1.3/Exe2.1.3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exe2.1.3
+//
+//  Created by Kyle on 16/4/16.
+//  Copyright © 2016年 Kyle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+    NSLog(@"Testing...\n..1\n...2\n....3");
+    }
+    return 0;
+}
