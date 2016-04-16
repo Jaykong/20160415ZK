af1883aa31de862ee3c58d890c94ec37d42e5e21
Kyle Zhang <zhk108@icloud.com>
2016-04-16 13:38:51 +0800

    Exe2.5

---
+++ Exercise/Chap2/Exe2.5/Exe2.5/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Exe2.5
+//
+//  Created by Kyle on 16/4/16.
+//  Copyright © 2016年 Kyle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int sum;
+        // COMPUTE RESULT
+        sum=25+37-19;
+        //DISPLAY RESULT
+        NSLog(@"The answer is %i",sum);
+    }
+    return 0;
+}
