b6fc3e25da1266a56f5f266dfa835fe614b2b2f6
Kyle Zhang <zhk108@icloud.com>
2016-04-16 13:41:53 +0800

    Exe2.6

---
+++ Exercise/Chap2/Exe2.6/Exe2.6/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  Exe2.6
+//
+//  Created by Kyle on 16/4/16.
+//  Copyright © 2016年 Kyle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int answer,result;
+        answer=100;
+        result=answer-10;
+    
+        
+        NSLog(@"The result is %i\n",result+5);
+    }
+    return 0;
+}
