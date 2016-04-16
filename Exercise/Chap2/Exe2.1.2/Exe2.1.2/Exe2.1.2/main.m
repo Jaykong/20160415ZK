065bb28cb5717b6a6c708f4e0d747fb4967d8550
Kyle Zhang <zhk108@icloud.com>
2016-04-16 12:22:14 +0800

    Exe2.1.2

---
+++ Exercise/Chap2/Exe2.1.2/Exe2.1.2/Exe2.1.2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Exe2.1.2
+//
+//  Created by Kyle on 16/4/16.
+//  Copyright © 2016年 Kyle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Programming is fun!");
+        NSLog(@"Programming in Objective-C is even more fun!");
+    }
+    return 0;
+}
