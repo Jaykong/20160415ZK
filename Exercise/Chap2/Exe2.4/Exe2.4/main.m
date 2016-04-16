ec2e8ad26306bf92a63b0e0abbc188d46f11dc95
Kyle Zhang <zhk108@icloud.com>
2016-04-16 13:36:36 +0800

    Exe2.4

---
+++ Exercise/Chap2/Exe2.4/Exe2.4/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Exe2.4
+//
+//  Created by Kyle on 16/4/16.
+//  Copyright © 2016年 Kyle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // 执行87减15并显示结果
+        int i=87,j=15,sum;
+        sum=i-j;
+        NSLog(@"87减15等于%i",sum);
+    }
+    return 0;
+}
