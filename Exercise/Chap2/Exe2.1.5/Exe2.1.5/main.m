e42597ae13c1e45a9ff0c259da7bfd4d0f0f116f
Kyle Zhang <zhk108@icloud.com>
2016-04-16 12:55:45 +0800

    Exe2.1.5

---
+++ Exercise/Chap2/Exe2.1.5/Exe2.1.5/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  Exe2.1.5
+//
+//  Created by Kyle on 16/4/16.
+//  Copyright © 2016年 Kyle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int value1,value2,sum;
+        
+        value1=50;
+        value2=25;
+        sum=value1+value2;
+        
+        NSLog(@"The sum of %i and %i is %d",value1,value2,sum);
+        
+        
+    }
+    return 0;
+}
