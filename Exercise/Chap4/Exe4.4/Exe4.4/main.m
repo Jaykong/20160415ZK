cda94888f1f21c1ffd456d6ca36dafbc0129e1cf
Kyle Zhang <zhk108@icloud.com>
2016-04-19 13:00:52 +0800

    Execise

---
+++ Exercise/Chap4/Exe4.4/Exe4.4/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  Exe4.4
+//
+//  Created by Kyle on 16/4/19.
+//  Copyright © 2016年 Kyle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        float x,sum;
+        
+        x=2.55;
+        sum=3*x*x*x-5*x*x+6;
+        
+        
+        NSLog(@"sum=%f",sum);
+    }
+    return 0;
+}
+//sum=23.231617
